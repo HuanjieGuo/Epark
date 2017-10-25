9b67dde47973accca38ff57a09a17d7608a9a6a5
hjackguo <hjackguo@163.com>
2017-10-25 16:05:42 +0800

    myBook->xib of cell

---
+++ ofoDemo/BookCellTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  BookCellTableViewCell.swift
+//  ofoDemo
+//
+//  Created by 郭焕杰 on 2017/10/25.
+//  Copyright © 2017年 hjackguo.github.io. All rights reserved.
+//
+
+import UIKit
+
+class BookCellTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var bookOperationView: MyProviewButton!
+    @IBOutlet weak var parkLocation: UILabel!
+
+    @IBOutlet weak var parkMoney: UILabel!
+    @IBOutlet weak var parkDuringTime: UILabel!
+    @IBAction func bookOperation(_ sender: Any) {
+    }
+    @IBOutlet weak var bookStatus: UILabel!
+    @IBOutlet weak var startDate: UILabel!
+    @IBOutlet weak var startTime: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
